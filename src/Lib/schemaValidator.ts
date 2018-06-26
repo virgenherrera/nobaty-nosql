34a8d6a6a3cebd278d43b1817501dc1620d7575e
hugo virgen <virgenherrera@gmail.com>
2018-06-26 15:59:47 -0500

    create Lib/schemaValidator

---
+++ src/Lib/schemaValidator.ts
@@ -0,0 +1,20 @@
+import * as Joi from 'joi';
+
+export function schemaValidator(params: any, schema: any, convert: boolean = false): any {
+	if (!schema) {
+		console.error(`Validation Schema was not provided!`);
+
+		throw { type: 500, msg: 'internal server error' };
+	}
+
+	const { error, value } = Joi.validate(params, schema, { convert });
+	if (error) {
+
+		console.error(error);
+
+		const [obj] = error.details;
+		throw { type: 400, msg: obj.message };
+	}
+
+	return value;
+}
