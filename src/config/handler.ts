6080755bb774ba9afae2a99e2332e7b7de8ed198
hugo virgen <virgenherrera@gmail.com>
2018-06-26 11:36:11 -0500

    drop users

--- src/config/handler.ts
+++ src/config/handler.ts
@@ -14,5 +14,4 @@ export { default as rendered_login } from '../Handler/Rendered/login';
 export { default as rendered_logout } from '../Handler/Rendered/logout';
 export { default as rendered_main } from '../Handler/Rendered/main';
 
-export { default as api_user } from '../Handler/Restful/user';
 export { default as api_login } from '../Handler/Restful/login';
