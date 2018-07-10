2cf841a9a06ecd4d680af5c989dbc5c64f1694e3
hugo virgen <virgenherrera@gmail.com>
2018-07-09 23:08:51 -0500

    test system/health inside a loop

--- test/api/system.test.ts
+++ test/api/system.test.ts
@@ -19,34 +19,35 @@ describe('System endpoints:', () => {
 		done();
 	});
 
+	for (let index = 0; index < 7; index++) {
+		it(`GET ${RoutePath.System_Health} attempt #${index + 1} should return 200`, (done) => {
+			const url = RoutePath.System_Health;
 
-	it('GET system/health/ should return 200', (done) => {
-		const url = RoutePath.System_Health;
+			request(app)
+				.get(url)
+				.expect(200)
+				.end((err, { body }) => {
+					if (err) {
+						return done(err);
+					}
 
-		request(app)
-			.get(url)
-			.expect(200)
-			.end((err, { body }) => {
-				if (err) {
-					return done(err);
-				}
+					expect(body).to.be.an('object');
+					expect(body).to.have.all.keys('message', 'data');
 
-				expect(body).to.be.an('object');
-				expect(body).to.have.all.keys('message', 'data');
+					expect(body.message).to.be.equal('Resource found');
 
-				expect(body.message).to.be.equal('Resource found');
+					expect(body.data).to.be.an('object');
+					expect(body.data).to.have.all.keys('uptime', 'humanUptime', 'uptimeSince', 'environment');
 
-				expect(body.data).to.be.an('object');
-				expect(body.data).to.have.all.keys('uptime', 'humanUptime', 'uptimeSince', 'environment');
+					expect(body.data.uptime).to.be.a('number');
+					expect(body.data.humanUptime).to.be.a('string');
+					expect(body.data.uptimeSince).to.be.a('string');
+					expect(body.data.environment).to.be.a('string');
+					expect(body.data.environment).to.be.equals(process.env.NODE_ENV);
 
-				expect(body.data.uptime).to.be.a('number');
-				expect(body.data.humanUptime).to.be.a('string');
-				expect(body.data.uptimeSince).to.be.a('string');
-				expect(body.data.environment).to.be.a('string');
-				expect(body.data.environment).to.be.equals(process.env.NODE_ENV);
-
-				done();
-			});
-	});
+					done();
+				});
+		});
+	}
 });
 
